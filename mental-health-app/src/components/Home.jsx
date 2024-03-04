dff4b4567cea4d5633d0ce2133566cf05fa11616
Iesha Ham <131932350+Ieshaham@users.noreply.github.com>
2024-03-04 21:48:48 +0000

    navbar created on homepage

---
+++ mental-health-app/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+        <div className="landing-page">
+           
+            <h1>SoulSupport</h1>
+            <img className="main-img" src="https://rtt.com/wp-content/uploads/2020/11/shocking-truth-about-1024x512.jpg" />
+        </div>
+        </div>
+    );
+};
+
+export default Home;
